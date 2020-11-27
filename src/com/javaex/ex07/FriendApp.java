37eae7a77f16f39e308ff82394aad6b07924dbb2
'cj2706' <'cj2706@naver.com'>
2020-11-27 18:20:19 +0900

    11/27

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -8,47 +8,39 @@ public class FriendApp {
 
         Friend[] friendArray = new Friend[3];
         Scanner sc = new Scanner(System.in);
-        
-        Friend friend = new Friend();
 
         System.out.println("친구를 3명 등록해 주세요");
-
-        //친구정보 3명 입력 로직 --> 반복문 사용
-            
-        	// 친구정보(이름) 입력받기
-
-        	// 친구정보(핸드폰) 입력받기
         
+        //친구정보 3명 입력 로직 --> 반복문 사용
+        for(int i = 0; i<friendArray.length; i++) {
+        	// 친구정보(이름) 입력받기
+        	System.out.print("이름: ");
+        	// 친구정보(핸드폰) 입력받기
+        	String name = sc.nextLine();
+        	System.out.print("핸드폰: ");
         	// 친구정보(학교) 입력받기
-
-            // Friend 인스턴스 생성하여 데이터 넣기
-
-            // 인스턴스 주소를 배열에 대입
-
+        	String hp = sc.nextLine();
+        	System.out.print("학교: ");
+        	String school = sc.nextLine();
+        	System.out.println("--------------------------------------------");
+        	
+        	// Friend 인스턴스 생성하여 데이터 넣기
+        	Friend frd = new Friend();
+        	
+        	frd.setName(name);
+        	frd.setHp(hp);
+        	frd.setSchool(school);
+        	
+        	// 인스턴스 주소를 배열에 대입
+        	friendArray[i] = frd;
+        }
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
-        	friendArray[i]=friend;
-        	
-        	for(int j=0; j<i; j++) {System.out.print("이름: ");
-        	friend.setName(sc.nextLine());
-        	
-        	System.out.print("핸드폰: ");
-        	friend.setHp(sc.nextLine());
-        	
-        	System.out.print("학교: ");
-        	friend.setSchool(sc.nextLine());
-        	
-        	
-        	System.out.println("------------------------------------");
-        	}
             //친구정보 출력 메소드 호출
-        }
-
-        for(int i = 0; i< friendArray.length; i++) {
         	friendArray[i].showInfo();
         }
-        
+
         sc.close();
     }
 
